b40e474189507b77918f1e851292c2ccee66b8f6
christopher cahill and robert lee <christopher@>
2019-01-31 13:31:07 -0800

    initialize repo with welcome page

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export const masterFirebaseConfig = {
+  apiKey: "AIzaSyCfV3UIADW5VRDyHNPyJHHgMfuxBzA29GY",
+  authDomain: "ng-crowdfunding.firebaseapp.com",
+  databaseURL: "https://ng-crowdfunding.firebaseio.com",
+  projectId: "ng-crowdfunding",
+  storageBucket: "",
+  messagingSenderId: "1032776543064"
+};
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { routing } from './app.routing';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
